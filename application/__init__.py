5fadeb48dd7b671a6c3e5241e3e8efbb158993b5
Nathan McCloskey <nathanmccloskey@gmail.com>
2015-01-21 21:34:26 -0500

    __init__.py

---
+++ application/__init__.py
@@ -0,0 +1,31 @@
+"""
+Initialize Flask app
+
+"""
+from flask import Flask
+import os
+from werkzeug.debug import DebuggedApplication
+
+app = Flask('application')
+
+if os.getenv('FLASK_CONF') == 'DEV':
+    # Development settings
+    app.config.from_object('application.settings.Development')
+    # Flask-DebugToolbar
+    toolbar = DebugToolbarExtension(app)
+    
+    @app.context_processor
+    def inject_profiler():
+        return dict(profiler_includes=templatetags.profiler_includes())
+    app.wsgi_app = profiler.ProfilerWSGIMiddleware(app.wsgi_app)
+
+elif os.getenv('FLASK_CONF') == 'TEST':
+    app.config.from_object('application.settings.Testing')
+
+else:
+    app.config.from_object('application.settings.Production')
+
+# Enable jinja2 loop controls extension
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+import views
+
