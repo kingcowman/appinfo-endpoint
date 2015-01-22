0ba2f6225116dba33cc8bbfcc46da80427d95039
Nathan McCloskey <nathanmccloskey@gmail.com>
2015-01-21 21:31:33 -0500

    application model

---
+++ application/models.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import ndb
+
+class Application(ndb.Model):
+  name = ndb.StringProperty()
+  type = ndb.StringProperty(repeated=True)
+  scope = ndb.StringProperty()
+  default_scopes = ndb.StringProperty(repeated=True)
+  icon_uri = ndb.StringProperty()
+  web_init_ep = ndb.StringProperty()
+  bundle_id = ndb.StringProperty()
+  custom_uri = ndb.StringProperty()
+  
