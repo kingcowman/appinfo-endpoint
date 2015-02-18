cd65258b8865f9f1bad55a9415b503a188afcb01
Cameron Spiller <camspilly@gmail.com>
2015-02-18 15:39:32 -0500

    added token, client, grant tokens

--- application/models.py
+++ application/models.py
@@ -15,3 +15,72 @@ class User(ndb.Model):
   email = ndb.StringProperty()
   userid = ndb.StringProperty()
   image = ndb.StringProperty()
+
+class Client(ndb.Model):
+  name = ndb.StringProperty()
+  description=ndb.StringProperty()
+  user_id = ndb.StringProperty()
+  client_id = ndb.StringProperty()
+  client_secret = ndb.StringProperty()
+  is_confident = ndb.BooleanProperty()
+
+  _redirect_uris = ndb.StringProperty()
+  _default_scopes = ndb.StringProperty(
+    )
+  @property
+  def client_type(self):
+    if self.is_confidential:
+    return 'confidential'
+  return 'public'
+
+  @property
+  def redirect_uris(self):
+  if self._redirect_uris:
+    return self._redirect_uris.split()
+  return []
+
+  @property
+  def default_redirect_uri(self):
+    return self.redirect_uris[0]
+
+  @property
+  def default_scopes(self):
+  if self._default_scopes:
+    return self._default_scopes.split()
+  return []
+
+  class Grant(ndb.Model):
+    id = ndb.IntegerProperty()
+    user_id = ndb.IntegerProperty()
+
+    client_id=ndb.StringProperty()
+
+    code = ndb.StringProperty()
+
+    redirect_uri = ndb.StringProperty()
+    expires = ndb.DateTimeProperty()
+
+    _scopes = ndb.StringProperty()
+    @property
+    def scopes(self):
+      if self._scopes:
+        return self._scopes.split()
+      return []
+
+  class Token(ndb.Model):
+    id = ndb.IntegerProperty()
+    client_id = ndb.StringProperty()
+    user_id = ndb.IntegerProperty()
+    token_type = ndb.StringProperty()
+    access_token = ndb.StringProperty()
+    refresh_token = ndb.StringProperty()
+    id_token = ndb.StringProperty()
+    expires = ndb.DateTimeProperty()
+
+    _scopes = ndb.StringProperty()
+
+    @property
+    def scopes(self):
+      if self._scopes:
+        return self._scopes.split()
+      return [] 
